2dc645e2172645bf41f7aadcaeb26b561cefa3a4
BobekRW <robertwyszecki@gmail.com>
2016-10-06 22:56:18 +0200

    new function (demage text)

--- Sieci Teleinformatyczne Lab1/Source.cpp
+++ Sieci Teleinformatyczne Lab1/Source.cpp
@@ -3,6 +3,7 @@ Sieci Teleinformatyczne
 Laboratorium nr 1
 dzieñ ostatniej aktualizacji 05.10 2016*/
 
+#pragma once
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -15,13 +16,18 @@ using namespace std;
 int main(){
 	string startText;				//Zmienna pomocnicza przechowuj¹ca wczytany na pocz¹tku tekst
 	string startBinary;				//Zmienna przechowuj¹ca tekst startowy zapisany za pomoc¹ kodu binarnego
-
-
-	openFile(startText);
-	saveFile(startText);
-
-
+	string demagedText;				//Zmienna przechowuj¹ca tekst startowy z b³êdem
+	int menuChoice = 0;				//Zmienna pomocnicza do wyboru opcji b³êdu, w menu programu
 	
+
+
+	openStartFile(startText);							//Funkcja otwieraj¹ca plik tekstowy i przypisuj¹ca go do zmiennej "startText"
+	saveBinaryFile(startText,startBinary);				//Funkcja zmieniaj¹ca tekst starttowy na kod binarny i zapisuj¹ca go do pliku i przypisuj¹ca go do zmiennej "startBinary"
+	menu(&menuChoice);									//menu programu
+	demageText(menuChoice, startBinary, demagedText);	//funkcja uszkadzaj¹ca plik startowy
+
+
+	cin.get();
 	cin.get();
 	return 0;
 }
--- Sieci Teleinformatyczne Lab1/function.h
+++ Sieci Teleinformatyczne Lab1/function.h
@@ -1,13 +1,15 @@
+#pragma once
 #include <iostream>
 #include <cstdlib>
 #include <string>
 #include <cmath>
 #include <fstream>
 #include <bitset>
+#include <ctime>
 
 using namespace std;
 
-void openFile(string &txt){
+void openStartFile(string &txt){
 	fstream plik;
 	plik.open("start.txt");
 	if (plik.good() == false){																	//Sprawdzenie czy odczytanie pliku siê powiod³o
@@ -22,7 +24,7 @@ void openFile(string &txt){
 	plik.close();
 }
 
-void saveFile(string In){
+void saveBinaryFile(string In, string &Out){
 
 	string tmp;
 	fstream plik;
@@ -30,16 +32,65 @@ void saveFile(string In){
 	for (std::size_t i = 0; i < In.size(); ++i)
 	{
 		plik << bitset<8>(In.c_str()[i]);
+		bitset<8> b(In.c_str()[i]);
+		Out += b.to_string();
 	}
 
-	
+
 
 	plik.close();
 }
 
-void changeToBinary(string In){
-	for (std::size_t i = 0; i < In.size(); ++i)
-	{
-		cout << bitset<8>(In.c_str()[i]);
+void bitParzystosci(string *in, string *out){
+	int suma = 0;
+
+}
+
+void menu(int *choice){
+	cout << "Prosze podac dla jakiego bledu przeslania wiadomosci, program ma zadzialac.(od 1% do 15%) " << endl;
+	cout << "Wybor: ";
+	cin >> *choice;
+	while (cin.good() == false || (*choice <1 || *choice >15)){
+		cout << "Podana wartosc jest niepoprawna prosze podac poprawna wartosc.\nWybor:";
+		cin.clear();
+		cin.sync();
+		cin >> *choice;
 	}
 }
+
+void demageText(int menuChoice, string In, string &Out){
+	int operations;
+	operations = In.size()*menuChoice / 100;
+	int tempNumber = 0;								//Zmienna pomocnicza do zapisania na chwile wylosowanej liczby
+	int numbers[35] = { 0 };						//tablica przechowuj¹ wylosowane liczby
+	int i = 0;										//zmienna przechowuj¹ca iloœæ liczb wylosowanych
+	int tmp = 0;									//Zmienna pomocnicza do zapisania na chwile numeru bitu, który ma byæ zmieniony 
+	srand(time(NULL));								//Zeruje zarodek
+	
+	while (i < operations){							
+		tempNumber = rand() % 200;
+		if (i == 0)
+			numbers[i] = tempNumber;
+		else{
+			for (int j = 0; j < i; j++){
+				while(numbers[j] == tempNumber){
+					tempNumber = rand() % 200;
+					j = 0;
+				}
+			}
+			numbers[i] = tempNumber;
+		}
+		i++;
+	}
+	
+	Out = In;
+
+	for (int j = 0; j < i; j++){
+		tmp = numbers[j];
+		if (Out[tmp] == '1')
+			Out[tmp] = '0';
+		else if (Out[tmp] == '0')
+			Out[tmp] = '1';
+	}
+}
+
