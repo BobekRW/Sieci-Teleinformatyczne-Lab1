1a066b78311b8ae632986c8f3e288340d8efa8c7
BobekRW <robertwyszecki@gmail.com>
2016-10-07 00:21:09 +0200

    build CRC function

--- Sieci Teleinformatyczne Lab1/Source.cpp
+++ Sieci Teleinformatyczne Lab1/Source.cpp
@@ -18,16 +18,17 @@ int main(){
 	string startBinary;				//Zmienna przechowuj¹ca tekst startowy zapisany za pomoc¹ kodu binarnego
 	string demagedText;				//Zmienna przechowuj¹ca tekst startowy z b³êdem
 	int menuChoice = 0;				//Zmienna pomocnicza do wyboru opcji b³êdu, w menu programu
+	int bitChoice = 0;				//Zmienna pomocnicza do wyboru ilosci bitow CRC
 	
 
 
 	openStartFile(startText);							//Funkcja otwieraj¹ca plik tekstowy i przypisuj¹ca go do zmiennej "startText"
 	saveBinaryFile(startText,startBinary);				//Funkcja zmieniaj¹ca tekst starttowy na kod binarny i zapisuj¹ca go do pliku i przypisuj¹ca go do zmiennej "startBinary"
-	menu(&menuChoice);									//menu programu
+	menu(&menuChoice, &bitChoice);						//menu programu
 	demageText(menuChoice, startBinary, demagedText);	//funkcja uszkadzaj¹ca plik startowy
+	bitParzystosci(startBinary, demagedText);			//funkcja sprawdzaj¹ca bit parzystosci
+	CRC(bitChoice);
 
-
-	cin.get();
-	cin.get();
+	system("pause");
 	return 0;
 }
--- Sieci Teleinformatyczne Lab1/function.h
+++ Sieci Teleinformatyczne Lab1/function.h
@@ -14,11 +14,11 @@ void openStartFile(string &txt){
 	plik.open("start.txt");
 	if (plik.good() == false){																	//Sprawdzenie czy odczytanie pliku siê powiod³o
 		cout << "Error!\n Can't open file\n";
-		exit(0);
+		exit(0);																				//jesli nie, wychodze z programu
 	}
-	else{
-		while (!plik.eof()){
-			getline(plik, txt);
+	else{																						//jesli plik istnieje	
+		while (!plik.eof()){																	//to do czasu az funkcja dojdzie do ostatniej linii
+			getline(plik, txt);																	//otwieram pobrana linie z pliku i zapisuje do zmiennej (w tym przypadku "txt")
 		}
 	}
 	plik.close();
@@ -41,24 +41,46 @@ void saveBinaryFile(string In, string &Out){
 	plik.close();
 }
 
-void bitParzystosci(string *in, string *out){
-	int suma = 0;
+void bitParzystosci(string In, string Out){
+	int suma1 = 0;
+	int suma2 = 0;
+	for (int i = 0; i < In.size(); i++){
+		if (In[i] == '1')
+			suma1 += 1;
+		if (Out[i] == '1')
+			suma2 += 1;
+	}
+	if (suma1 == suma2)
+		cout << "Wiadomosc wyslana poprawnie" << endl;
+	else
+		cout << "Wiadomosc zostala uszkodzona podczas przesylania" << endl;
 
 }
 
-void menu(int *choice){
-	cout << "Prosze podac dla jakiego bledu przeslania wiadomosci, program ma zadzialac.(od 1% do 15%) " << endl;
+void menu(int *fchoice, int *bchoice){
+	cout << "Prosze podac dla jakiego bledu przeslania wiadomosci, program ma zadzialac.(od 0% do 15%) " << endl;
 	cout << "Wybor: ";
-	cin >> *choice;
-	while (cin.good() == false || (*choice <1 || *choice >15)){
+	cin >> *fchoice;
+	while (cin.good() == false || (*fchoice <0 || *fchoice >15)){
 		cout << "Podana wartosc jest niepoprawna prosze podac poprawna wartosc.\nWybor:";
 		cin.clear();
 		cin.sync();
-		cin >> *choice;
+		cin >> *fchoice;
+	}
+
+	cout << "Prosze podac dla ilu bitow program ma przeprowadzic CRC (max 32)" << endl;
+	cout << "Wybor: ";
+	cin >> *bchoice;
+	while (cin.good() == false || (*bchoice <0 || *bchoice >32)){
+		cout << "Podana wartosc jest niepoprawna prosze podac poprawna wartosc.\nWybor:";
+		cin.clear();
+		cin.sync();
+		cin >> *bchoice;
 	}
 }
 
 void demageText(int menuChoice, string In, string &Out){
+	Out = In;
 	int operations;
 	operations = In.size()*menuChoice / 100;
 	int tempNumber = 0;								//Zmienna pomocnicza do zapisania na chwile wylosowanej liczby
@@ -66,31 +88,49 @@ void demageText(int menuChoice, string In, string &Out){
 	int i = 0;										//zmienna przechowuj¹ca iloœæ liczb wylosowanych
 	int tmp = 0;									//Zmienna pomocnicza do zapisania na chwile numeru bitu, który ma byæ zmieniony 
 	srand(time(NULL));								//Zeruje zarodek
+
 	
 	while (i < operations){							
-		tempNumber = rand() % 200;
-		if (i == 0)
+		tempNumber = rand() % 200;								//Losuje liczbe
+		if (i == 0)												//jesli licznik równy 0, zapisuje liczbe
 			numbers[i] = tempNumber;
-		else{
-			for (int j = 0; j < i; j++){
-				while(numbers[j] == tempNumber){
+		else{													//jesli nie
+			for (int j = 0; j < i; j++){						//sprawdzam czy juz znajduje sie w tablicy wylosowanych liczb
+				while(numbers[j] == tempNumber){				//jesli sie znajduje, losuje kolejna liczbe
 					tempNumber = rand() % 200;
-					j = 0;
+					j = 0;										//a for'a zeruje
 				}
 			}
-			numbers[i] = tempNumber;
+			numbers[i] = tempNumber;							//przypisuje wylosowna liczbe, do tablicy liczb wylosowanych
 		}
-		i++;
+		i++;													//implementuje licznik
 	}
 	
-	Out = In;
-
-	for (int j = 0; j < i; j++){
-		tmp = numbers[j];
-		if (Out[tmp] == '1')
-			Out[tmp] = '0';
+	for (int j = 0; j < i; j++){								//petla do uszkadzania tekstu
+		tmp = numbers[j];										//przypisanie do zmiennej pomocniczej nr bitu, który ma zostac zmieniony
+		if (Out[tmp] == '1')					
+			Out[tmp] = '0';									
 		else if (Out[tmp] == '0')
 			Out[tmp] = '1';
 	}
 }
 
+void CRC(int bChoice){
+	char dzielnikCRC[33] = { 0 };
+	srand(time(NULL));
+	for(int i = 0; i < bChoice+1; i++){
+		if (i == 0 || i == bChoice)								//Warunek konieczny aby na poczatku i koncu dzielnika by³a 1
+			dzielnikCRC[i] = '1';											
+		else{
+			if (rand() % 4 >1)
+				dzielnikCRC[i] = '1';
+			else
+				dzielnikCRC[i] = '0';
+		}
+	}
+	
+	for (int i = 0; i < bChoice+1; i++){
+		cout << dzielnikCRC[i];
+	}
+	cout << endl;
+}
